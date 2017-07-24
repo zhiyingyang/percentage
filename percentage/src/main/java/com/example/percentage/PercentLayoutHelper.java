8b47394c73d934ef10b8b2db4c642f2f459d41fe
zhiyingyang <1259061023@qq.com>
2017-07-24 17:34:02 +0800

    百分比布局

--- percentage/src/main/java/com/example/percentage/PercentLayoutHelper.java
+++ percentage/src/main/java/com/example/percentage/PercentLayoutHelper.java
@@ -30,7 +30,6 @@ import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
 
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.regex.Matcher;
